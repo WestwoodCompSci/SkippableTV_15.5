2b26fb922bd80f398f58c0685190ff142ef4e885
compscifan6336 <alanrayshaw@gmail.com>
2015-04-23 10:39:12 -0500

    classes

--- src/SkippableTVGui/homeScreen.java
+++ src/SkippableTVGui/homeScreen.java
@@ -1,7 +1,10 @@
 package SkippableTVGui;
-import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import java.awt.Dimension;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class homeScreen {
 	
@@ -12,13 +15,21 @@ public class homeScreen {
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(0,0);
 		myFrame.setPreferredSize(new Dimension(1440,850));
-		
+		JButton testButton = new JButton("test");
+		myFrame.add(testButton);
 		myFrame.pack();
 		myFrame.setVisible(true);	
+	
+	
+		testButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				new userHomeScreen();	
+				myFrame.dispose();
+			}
+		});
 	}
-	
-	
-	
 	public static void main(String[] args)
 	{
 		SwingUtilities.invokeLater(new Runnable() {
--- src/SkippableTVGui/userHomeScreen.java
+++ src/SkippableTVGui/userHomeScreen.java
@@ -1,5 +1,19 @@
 package SkippableTVGui;
 
-public class userHomeScreen {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class userHomeScreen {
+	private JFrame myFrame;
+	
+	public userHomeScreen(){
+		myFrame = new JFrame("SkippableTV");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setLocation(0,0);
+		myFrame.setPreferredSize(new Dimension(1440,850));
+		
+		myFrame.pack();
+		myFrame.setVisible(true);	
+	}
 }
