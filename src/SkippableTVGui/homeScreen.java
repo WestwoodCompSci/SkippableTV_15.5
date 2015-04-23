f3bfa042ce16395c59fc824486eaf831ba059943
compscifan6336 <alanrayshaw@gmail.com>
2015-04-23 10:26:01 -0500

    Created classes

--- src/SkippableTVGui/homeScreen.java
+++ src/SkippableTVGui/homeScreen.java
@@ -1,35 +1,28 @@
+package SkippableTVGui;
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-
-public class SkippableTVGUI {
-
+public class homeScreen {
+	
 	private JFrame myFrame;
 	
-	public SkippableTVGUI()
-	{
-
+	public homeScreen(){
 		myFrame = new JFrame("SkippableTV");
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(0,0);
 		myFrame.setPreferredSize(new Dimension(1440,850));
 		
-		
-		
-		
-				
 		myFrame.pack();
 		myFrame.setVisible(true);	
 	}
 	
 	
+	
 	public static void main(String[] args)
 	{
 		SwingUtilities.invokeLater(new Runnable() {
-			public void run() { new SkippableTVGUI(); }
+			public void run() { new homeScreen(); }
 		});
 	}
-
 }
---
+++ src/SkippableTVGui/userHomeScreen.java
@@ -0,0 +1,5 @@
+package SkippableTVGui;
+
+public class userHomeScreen {
+
+}
