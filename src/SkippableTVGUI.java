116a0d512fea0978a759d691ed673171cdfd2d01
toStringDong <jenlidong98@gmail.com>
2015-04-23 10:15:58 -0500

    Frame created

--- src/SkippableTVGUI.java
+++ src/SkippableTVGUI.java
@@ -1,4 +1,35 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 
 public class SkippableTVGUI {
+
+	private JFrame myFrame;
+	
+	public SkippableTVGUI()
+	{
+
+		myFrame = new JFrame("SkippableTV");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setLocation(0,0);
+		myFrame.setPreferredSize(new Dimension(1440,850));
+		
+		
+		
+		
+				
+		myFrame.pack();
+		myFrame.setVisible(true);	
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() { new SkippableTVGUI(); }
+		});
+	}
 
 }
