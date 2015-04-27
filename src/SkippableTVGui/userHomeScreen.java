78543f0481dc4684cd5547c2a584ae6e3555e623
compscifan6336 <alanrayshaw@gmail.com>
2015-04-27 09:55:42 -0500

    added logo

--- src/SkippableTVGui/userHomeScreen.java
+++ src/SkippableTVGui/userHomeScreen.java
@@ -1,6 +1,12 @@
 package SkippableTVGui;
 import java.awt.Dimension;
+import java.awt.Image;
+
+import javax.swing.Box;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class userHomeScreen {
@@ -11,6 +17,12 @@ public class userHomeScreen {
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(0,0);
 		myFrame.setPreferredSize(new Dimension(1440,850));
+		
+		Icon logo = new ImageIcon("skippy.png");
+		JLabel logoLabel = new JLabel(logo);
+		Box topBox = Box.createHorizontalBox();
+		topBox.add(logoLabel);
+		myFrame.add(topBox);
 		
 		myFrame.pack();
 		myFrame.setVisible(true);	
