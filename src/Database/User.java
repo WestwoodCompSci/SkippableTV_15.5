b299e4977beef1ff3cdd0f4fc7c97b22e661adc2
Noelle516 <noelle.rivera.516@gmail.com>
2015-04-27 09:50:51 -0500

    User Draft #1

--- src/Database/User.java
+++ src/Database/User.java
@@ -1,4 +1,45 @@
 package Database;
+
+import java.util.List;
+
 public class User {
 
+	private String name;
+	private String password;
+	private List<Show> myShows;
+	private String email;
+	
+	public User()
+	{
+		
+	}
+	
+	public String addPassword(String password)
+	{
+		String thing = password;
+		//encrypt thing
+		return thing;
+		
+	}
+	
+	public String addUsername(String username)
+	{
+		String thing = username;
+		//encrypt thing
+		return thing;
+	}
+	
+	public String addUserEmail(String email)
+	{
+		String thing = email;
+		//encrypt email
+		return thing;
+	}
+	
+	public String addShow(Show show)
+	{
+		
+	}
+	
+	
 }
