4b328ca029850888ab8f1a7f3a2d5b4aea76cbf1
ajboss98 <aj280598@gmail.com>
2015-04-27 10:35:19 -0500

    more updates!

--- src/DataEntry.java
+++ src/DataEntry.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -42,7 +43,12 @@ public class DataEntry {
 			 		FileReader reader = new FileReader("H:\\CompSci3\\SkippableTV_15.5\\sherlock.json");
 			 		JSONParser parser = new JSONParser();
 			 		JSONObject jsonObj = (JSONObject) parser.parse(reader);
-			 		
+			 		String showname = (String) jsonObj.get("epguide_name");
+			 		//update database class
+			 		int numofseasons;
+			 		List<Integer> seasons = null;
+			 		seasons.add((Integer) jsonObj.get("season"));
+			 		for(int i=0; i<) 	
 				 }
 		}catch(Exception e){
 			
