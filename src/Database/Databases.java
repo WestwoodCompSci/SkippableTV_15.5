a082a458462fbae6052fa4a229a5255fd9047672
DAVlD <chaod52@gmail.com>
2015-04-27 09:51:08 -0500

    my data

--- src/Database/Databases.java
+++ src/Database/Databases.java
@@ -1,5 +1,42 @@
 package Database;
+import java.util.*;
+public class Databases 
+{
+	//FIELDS TODO
+	private String Username;
+	private String Password;
+	private String name;
+	public void Database()
+	{
+		Username="";
+		Password="";
+		name="";
+	}
+	
+	public String lookupPassword(String Password)
+	{
+		return Password;
+		
+	}
+	public String lookupUsername(String Username)
+	{
+		return Username;
 
-public class Databases {
+	}
+	
 
+	public String getUser(String Username, String password)
+	{
+		return Username;
+	}
+	public String getShow(String name)
+	{
+		return name;
+	}
+	
+	
+	
+	
+	
+	
 }
