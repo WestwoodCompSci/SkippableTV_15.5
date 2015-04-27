bd5f22de7b326df86a5322b67f60a90b47edce81
Whitneyoaks11 <cody13337@gmail.com>
2015-04-27 10:09:13 -0500

    Shows Class

--- src/Database/Show.java
+++ src/Database/Show.java
@@ -1,6 +1,66 @@
 package Database;
 
-
 public class Show {
-
+	
+	//FIELDS
+	//---------------------------------------------------------------
+	private String myName;
+	private String myInfo;
+	private int myNumOfSeasons;
+	private List<String> myEpisodes;
+	private int mySeason;
+	public String getName()
+	
+	//CONSTRUCTORS
+	//----------------------------------------------------------------
+	public Show()
+	{
+		
+	}
+	//METHODS
+	//----------------------------------------------------------------
+	
+	{
+		return myName;
+	}
+	
+	public String getInfo()
+	{
+		return myInfo;
+	}
+	
+	public int getNumOfSeasons()
+	{
+		return myNumOfSeasons;
+	}
+	
+	public List<String> getEpisodes()
+	{
+		return myEpisodes;
+	}
+	
+	public List<String> getEpisodes(int season)
+	{
+		
+	}
+	
+	public List<String> getComments(String username)
+	{
+		
+	}
+	
+	public void isSeason(int season)
+	{
+		return mySeason;
+	}
+	
+	public Season getSeason(int num)
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
 }
