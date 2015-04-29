b45e89efe9b50fbeef0b4956e47ce5117e8cd99f
toStringDong <jenlidong98@gmail.com>
2015-04-29 09:19:00 -0500

    GUI update

--- src/SkippableTVGui/loginScreen.java
+++ src/SkippableTVGui/loginScreen.java
@@ -1,9 +1,20 @@
 package SkippableTVGui;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.Box;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -17,32 +28,74 @@ public class loginScreen {
 	{
 		myLoginFrame = new JFrame("SkippableTV");
 		myLoginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myLoginFrame.setLocation(0,0);
-		myLoginFrame.setPreferredSize(new Dimension(1440,850));
+		myLoginFrame.setLocation(400,250);
+		myLoginFrame.setPreferredSize(new Dimension(500,550));
+		
+		
+		
 		
 		Box box = Box.createVerticalBox();
 		myLoginFrame.add(box);
 		
+		//Icon logo = new ImageIcon("skippy.png");
+		//logo.setPreferredWidth(30);
+		BufferedImage img;
+		try {
+			img = ImageIO.read(new File("skippy.png"));
+			
+			JLabel hi = new JLabel(new ImageIcon(img.getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
+			
+			box.add(hi);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//resize here
+	
+	
+		
 		box.add(Box.createVerticalStrut(30));
 		
 		Box h1Box = Box.createHorizontalBox();
-		JLabel Username = new JLabel("	Username");
+		JLabel Username = new JLabel("Username  ");
 		Username.setFont(new Font("Avenir",Font.PLAIN,22));
 		h1Box.add(Username);
 		JTextField usernameText = new JTextField();
 		h1Box.add(usernameText);
 		
+		
 		box.add(h1Box);
 		
 		Box h2Box = Box.createHorizontalBox();
-		JLabel Password = new JLabel("	Password");
+		JLabel Password = new JLabel("Password   ");
 		Password.setFont(new Font("Avenir",Font.PLAIN,22));
 		h2Box.add(Password);
 		JTextField passwordText = new JTextField();
 		h2Box.add(passwordText);
 		
+		
 		box.add(h2Box);
 		
+		JButton login = new JButton("Login");
+		box.add(login);
+		
+		
+		login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username = usernameText.getText();
+				String password = passwordText.getText();
+				
+				//Server server = new Server();
+				//boolean valid = server.authenticate(username,password);
+				
+				//if(valid == true)
+				{
+					
+				}
+				
+			}
+		});
 		
 		
 		myLoginFrame.pack();
