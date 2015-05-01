6cd4a24773b7085f1554234d636af1d5071a90ec
DAVlD <chaod52@gmail.com>
2015-05-01 10:07:40 -0500

    hdfhsdf

--- src/Database/Databases.java
+++ src/Database/Databases.java
@@ -1,8 +1,10 @@
 package Database;
 import java.util.*;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 
@@ -47,11 +49,85 @@ public class Databases
 	
 	public void appendtoDatabaseFile(Show myShow,Season mySeason,Episode myEpisode)
 	{
-		File database=new File("Database.txt");
-		
+		try {
+			 
+			myEpisode=new Episode();
+ 
+			File file = new File("database.txt");
+ 
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(myEpisode.getEpisodeName());
+			bw.write(myEpisode.getEpisodeInfo());
+			bw.write(myEpisode.getSkippability());
+			bw.write(myEpisode.getNumEpisode());
+			bw.close();
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+		try {
+			 
+			mySeason=new Season();
+ 
+			File file = new File("database.txt");
+ 
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(mySeason.getComments(username));
+			bw.write(mySeason.getEpisodes(List<String>thing));
+			
+			bw.write(mySeason.getInfo());
+
+			bw.close();
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			 
+			myShow=new Show(Password, Password, 0, null, 0, null);
+ 
+			File file = new File("database.txt");
+ 
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(myShow.getName());
+			bw.write(myShow.getInfo());
+			bw.write(myShow.getNumOfSeasons());
+			bw.write(myShow.getEpisodes(List<String>Show));
+			bw.write(myShow.getSeason(0));
+			bw.write(myShow.getComments(Username));
+			bw.close();
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
 	}
 	
-	
-	
-	
-}
+
